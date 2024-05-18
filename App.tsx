a7efd63d3e49695f1a9b054b63d2f48bc1a85690
Luks17 <lucasthidavort@gmail.com>
2024-05-18 19:33:21 -0400

    Added react query

--- App.tsx
+++ App.tsx
@@ -6,19 +6,24 @@ import LoginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
 import AppScreen from "./screens/AppScreen";
 import AuthContextProvider from "./store/auth-context";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <AuthContextProvider>
-      <StatusBar style="auto" />
-      <NavigationContainer>
-        <RootStack.Navigator>
-          <RootStack.Screen name="Home" component={HomeScreen} />
-          <RootStack.Screen name="Login" component={LoginScreen} />
-          <RootStack.Screen name="SignUp" component={SignupScreen} />
-          <RootStack.Screen name="App" component={AppScreen} />
-        </RootStack.Navigator>
-      </NavigationContainer>
-    </AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <StatusBar style="auto" />
+        <NavigationContainer>
+          <RootStack.Navigator>
+            <RootStack.Screen name="Home" component={HomeScreen} />
+            <RootStack.Screen name="Login" component={LoginScreen} />
+            <RootStack.Screen name="SignUp" component={SignupScreen} />
+            <RootStack.Screen name="App" component={AppScreen} />
+          </RootStack.Navigator>
+        </NavigationContainer>
+      </AuthContextProvider>
+    </QueryClientProvider>
   );
 }
