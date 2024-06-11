7b19d10f6b848c341924621f21eeb70e18c03d3f
Luks17 <lucasthidavort@gmail.com>
2024-06-11 17:57:48 -0400

    Success and error toasts for add song

--- components/core/AddSong.tsx
+++ components/core/AddSong.tsx
@@ -1,17 +1,27 @@
 import { StyleSheet, Text, View } from "react-native";
 import IconBtn from "../buttons/IconBtn";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { colors } from "../../lib/constants/colors";
 import InputField from "../forms/InputField";
 import PrimaryButton from "../buttons/PrimaryBtn";
 import SlideUpModal from "../modals/SlideUpModal";
 import { useAddSongQuery } from "../../lib/network/songs/useAddSongQuery";
+import { ToastContext } from "../../store/toast-context";
 
 function AddSong() {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [songUrl, setSongUrl] = useState("");
 
-  const { mutate, data, isError, failureReason, isPending } = useAddSongQuery();
+  const { displayToast } = useContext(ToastContext);
+
+  const { mutate, isError, failureReason, isPending, isSuccess } =
+    useAddSongQuery();
+
+  useEffect(() => {
+    if (isSuccess) displayToast("success", "Música adicionada com sucesso");
+    else if (isError && failureReason)
+      displayToast("error", failureReason.message);
+  }, [isSuccess, isError]);
 
   const showModal = () => setIsModalVisible(true);
   const hideModal = () => setIsModalVisible(false);
--- lib/network/error.ts
+++ lib/network/error.ts
@@ -1,6 +1,7 @@
 export enum ErrorTypes {
   LOGIN_FAIL = "Usuário ou senha inválido(s)...",
   USERNAME_ALREADY_USED = "Um usuário com esse nome já existe no banco de dados!",
+  SERVICE_ERROR = "Houve um problema no servidor, tente novamente mais tarde!",
 }
 
 export class ClientError extends Error {
